40a89094e25b9ed58c8e4a8d61508bfb0e4c2250
Sannithi Balaji <sannithibalaji6@gmail.com>
2020-12-27 21:54:51 +0530

    a

--- opsworks/recipes/setup.rb
+++ opsworks/recipes/setup.rb
@@ -1,28 +1,28 @@
-include_recipe 'tar'
-directory '/opt/sources' do
-    owner 'root'
-    group 'root'
-    mode '0755'
+include_recipe "tar"
+directory "/opt/sources" do
+    owner "root"
+    group "root"
+    mode "0755"
     action :create
 end
 
-chef_version= default['cockroach_version']
-remote_file '/opt/sources/cockroach-${chef_version}.linux-amd64.tgz' do
-  source 'https://binaries.cockroachdb.com/cockroach-${chef_version}.linux-amd64.tgz'
+cockroach_version= default["cockroach_version"]
+remote_file "/opt/sources/cockroach-#{cockroach_version}.linux-amd64.tgz" do
+  source "https://binaries.cockroachdb.com/cockroach-#{cockroach_version}.linux-amd64.tgz"
   action :create
 end
 
 
-tar_extract '/opt/sources/cockroach-${chef_version}.linux-amd64.tgz' do
+tar_extract "/opt/sources/cockroach-#{cockroach_version}.linux-amd64.tgz" do
   action :extract_local
-  target_dir '/opt/sources'
-  creates '/opt/sources/cockroach-${chef_version}.linux-amd64/cockroach'
+  target_dir "/opt/sources"
+  creates "/opt/sources/cockroach-#{cockroach_version}.linux-amd64/cockroach"
 end
 
 remote_file "Copy executable" do 
   path "/usr/local/bin/cockroach" 
-  source "file:///opt/sources/cockroach-${chef_version}.linux-amd64/cockroach"
-  owner 'root'
-  group 'root'
+  source "file:///opt/sources/cockroach-#{cockroach_version}.linux-amd64/cockroach"
+  owner "root"
+  group "root"
   mode 0755
 end
